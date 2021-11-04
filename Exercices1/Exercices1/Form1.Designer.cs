cd1dae3458676ddc588aea340de0da30730d59fd
Joel Kamo <thibeau.kamo@gmail.com>
2021-11-04 11:50:14 -0400

    version 1

---
+++ Exercices1/Exercices1/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace Exercices1
+{
+    partial class cmdTexte1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdTNRG14 = new System.Windows.Forms.Button();
+            this.lblAffichage = new System.Windows.Forms.Label();
+            this.cmdArialG14 = new System.Windows.Forms.Button();
+            this.cmdScriptG18 = new System.Windows.Forms.Button();
+            this.cmdArialG24 = new System.Windows.Forms.Button();
+            this.cmdTNRG24 = new System.Windows.Forms.Button();
+            this.cmdScriptG36 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmdTNRG14
+            // 
+            this.cmdTNRG14.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cmdTNRG14.ForeColor = System.Drawing.Color.White;
+            this.cmdTNRG14.Location = new System.Drawing.Point(54, 128);
+            this.cmdTNRG14.Name = "cmdTNRG14";
+            this.cmdTNRG14.Size = new System.Drawing.Size(300, 60);
+            this.cmdTNRG14.TabIndex = 0;
+            this.cmdTNRG14.Text = "Times New Roman - Gras - 14";
+            this.cmdTNRG14.UseVisualStyleBackColor = true;
+            this.cmdTNRG14.Click += new System.EventHandler(this.cmdTNRG14_Click);
+            // 
+            // lblAffichage
+            // 
+            this.lblAffichage.AutoSize = true;
+            this.lblAffichage.ForeColor = System.Drawing.Color.White;
+            this.lblAffichage.Location = new System.Drawing.Point(314, 49);
+            this.lblAffichage.Name = "lblAffichage";
+            this.lblAffichage.Size = new System.Drawing.Size(109, 15);
+            this.lblAffichage.TabIndex = 1;
+            this.lblAffichage.Text = "Programmation C#";
+            this.lblAffichage.Click += new System.EventHandler(this.lblAffichage_Click);
+            // 
+            // cmdArialG14
+            // 
+            this.cmdArialG14.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cmdArialG14.ForeColor = System.Drawing.Color.White;
+            this.cmdArialG14.Location = new System.Drawing.Point(54, 231);
+            this.cmdArialG14.Name = "cmdArialG14";
+            this.cmdArialG14.Size = new System.Drawing.Size(300, 60);
+            this.cmdArialG14.TabIndex = 2;
+            this.cmdArialG14.Text = "Arial - Gras - 14";
+            this.cmdArialG14.UseVisualStyleBackColor = true;
+            this.cmdArialG14.Click += new System.EventHandler(this.cmdArialG14_Click);
+            // 
+            // cmdScriptG18
+            // 
+            this.cmdScriptG18.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.cmdScriptG18.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cmdScriptG18.ForeColor = System.Drawing.Color.White;
+            this.cmdScriptG18.Location = new System.Drawing.Point(54, 342);
+            this.cmdScriptG18.Name = "cmdScriptG18";
+            this.cmdScriptG18.Size = new System.Drawing.Size(300, 60);
+            this.cmdScriptG18.TabIndex = 3;
+            this.cmdScriptG18.Text = "Script MT Bold - Gras - 18";
+            this.cmdScriptG18.UseVisualStyleBackColor = true;
+            this.cmdScriptG18.Click += new System.EventHandler(this.cmdScriptG18_Click);
+            // 
+            // cmdArialG24
+            // 
+            this.cmdArialG24.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.cmdArialG24.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cmdArialG24.ForeColor = System.Drawing.Color.White;
+            this.cmdArialG24.Location = new System.Drawing.Point(390, 233);
+            this.cmdArialG24.Name = "cmdArialG24";
+            this.cmdArialG24.Size = new System.Drawing.Size(297, 58);
+            this.cmdArialG24.TabIndex = 4;
+            this.cmdArialG24.Text = "Arial - Gras - 24";
+            this.cmdArialG24.UseVisualStyleBackColor = true;
+            this.cmdArialG24.Click += new System.EventHandler(this.cmdArialG24_Click);
+            // 
+            // cmdTNRG24
+            // 
+            this.cmdTNRG24.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.cmdTNRG24.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cmdTNRG24.ForeColor = System.Drawing.Color.White;
+            this.cmdTNRG24.Location = new System.Drawing.Point(390, 128);
+            this.cmdTNRG24.Name = "cmdTNRG24";
+            this.cmdTNRG24.Size = new System.Drawing.Size(297, 60);
+            this.cmdTNRG24.TabIndex = 5;
+            this.cmdTNRG24.Text = "Times New Roman - Gras - 24";
+            this.cmdTNRG24.UseVisualStyleBackColor = true;
+            this.cmdTNRG24.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmdScriptG36
+            // 
+            this.cmdScriptG36.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.cmdScriptG36.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cmdScriptG36.ForeColor = System.Drawing.Color.White;
+            this.cmdScriptG36.Location = new System.Drawing.Point(390, 342);
+            this.cmdScriptG36.Name = "cmdScriptG36";
+            this.cmdScriptG36.Size = new System.Drawing.Size(297, 60);
+            this.cmdScriptG36.TabIndex = 6;
+            this.cmdScriptG36.Text = "Script MT Bold - Gras - 36";
+            this.cmdScriptG36.UseVisualStyleBackColor = true;
+            this.cmdScriptG36.Click += new System.EventHandler(this.cmdScriptG36_Click);
+            // 
+            // cmdTexte1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(771, 450);
+            this.Controls.Add(this.cmdScriptG36);
+            this.Controls.Add(this.cmdTNRG24);
+            this.Controls.Add(this.cmdArialG24);
+            this.Controls.Add(this.cmdScriptG18);
+            this.Controls.Add(this.cmdArialG14);
+            this.Controls.Add(this.lblAffichage);
+            this.Controls.Add(this.cmdTNRG14);
+            this.Name = "cmdTexte1";
+            this.Text = "Texte1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdTNRG14;
+        private System.Windows.Forms.Label lblAffichage;
+        private System.Windows.Forms.Button cmdArialG14;
+        private System.Windows.Forms.Button cmdScriptG18;
+        private System.Windows.Forms.Button cmdArialG24;
+        private System.Windows.Forms.Button cmdTNRG24;
+        private System.Windows.Forms.Button cmdScriptG36;
+    }
+}
+
---
+++ Exercices1/Exercices1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercices1
+{
+    public partial class cmdTexte1 : Form
+    {
+        public cmdTexte1()
+        {
+            InitializeComponent();
+            lblAffichage.Visible = false;
+        }
+
+        private void cmdTNRG14_Click(object sender, EventArgs e)
+        {
+            FontFamily TNR = new FontFamily("Times New Roman");
+            lblAffichage.Font = new Font(TNR, 14, FontStyle.Bold);
+            lblAffichage.Visible = true;
+        }
+
+        private void cmdArialG14_Click(object sender, EventArgs e)
+        {
+            FontFamily ARL = new FontFamily("Arial");
+            lblAffichage.Font = new Font(ARL, 14, FontStyle.Bold);
+            lblAffichage.Visible = true;
+
+        }
+
+        private void cmdScriptG18_Click(object sender, EventArgs e)
+        {
+            FontFamily SPT = new FontFamily("Script MT Bold");
+            lblAffichage.Font = new Font(SPT, 18, FontStyle.Bold);
+            lblAffichage.Visible = true;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FontFamily TNR = new FontFamily("Times New Roman");
+            lblAffichage.Font = new Font(TNR, 24, FontStyle.Bold);
+            lblAffichage.Visible = true;
+        }
+
+        private void cmdArialG24_Click(object sender, EventArgs e)
+        {
+            FontFamily ARL = new FontFamily("Arial");
+            lblAffichage.Font = new Font(ARL, 24, FontStyle.Bold);
+            lblAffichage.Visible = true;
+        }
+
+        private void cmdScriptG36_Click(object sender, EventArgs e)
+        {
+            FontFamily SPT = new FontFamily("Script MT Bold");
+            lblAffichage.Font = new Font(SPT, 36, FontStyle.Bold);
+            lblAffichage.Visible = true;
+        }
+
+        private void lblAffichage_Click(object sender, EventArgs e)
+        {
+            lblAffichage.Visible = false;
+        }
+    }
+}
