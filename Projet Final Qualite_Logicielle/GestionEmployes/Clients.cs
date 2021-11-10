595519b3fa852037e09f70c15ff72aac185540cb
Joel Kamo <thibeau.kamo@gmail.com>
2021-11-09 21:10:13 -0500

    Version 1.0

---
+++ Projet Final Qualite_Logicielle/GestionEmployes/Clients.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionFacturesClients
+{
+    class Clients
+    {
+
+        public int ID
+        {
+            get; set;
+        }
+
+        public string Libelle { get; set; }
+        public static int cmp = 0;
+
+        public Clients ()
+        {
+            cmp++;
+            this.ID = cmp;
+        }
+
+        public Clients(String Libelle)
+        {
+            cmp++;
+            this.ID = cmp;
+            this.Libelle = Libelle;
+        }
+    }
+}
---
+++ Projet Final Qualite_Logicielle/GestionEmployes/Facture.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionFacturesClients
+{
+    class Facture
+    {
+
+        public int ID
+        {
+            get; set;
+        }
+
+        public string NoFacture { get; set; }
+
+        //Caracteristique de l'objet Facture
+
+        public string NomArticle { get; set; }
+        public int QuantiteArticle { get; set; }
+        public double PrixArticle { get; set; }
+        public  double TotalArticle { get; set; }
+
+        public static int cmp = 0;
+
+        public Facture()
+        {
+            cmp++;
+            this.ID = cmp;
+        }
+
+        public Facture(String NoFacture)
+        {
+            cmp++;
+            this.ID = cmp;
+            this.NoFacture = NoFacture;
+        }
+
+
+    }
+}
