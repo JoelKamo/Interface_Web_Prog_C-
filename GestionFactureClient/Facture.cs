ac919210797f82e50e674504c95064469362316a
Joel Kamo <thibeau.kamo@gmail.com>
2021-11-22 09:15:49 -0500

    Essai

---
+++ GestionFactureClient/Facture.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionFactureClient
+{
+    class Facture
+    {
+
+        public int ID
+        {
+            get; set;
+        }
+
+        public string NoFacture { get; set; }
+
+        //Caracteristique de l'objet Facture
+
+
+        public static List<Contact> NewContact = new List<Contact>();
+        
+
+        //public int QuantiteArticle { get; set; }
+        public string NomArticle { get; set; }
+        public double PrixArticle { get; set; }
+        public string TypeClients { get; set; }
+
+        public Contact C { get; set; }
+
+        public static int cmp = 0;
+
+        public Facture()
+        {
+            cmp++;
+            this.ID = cmp;
+        }
+
+        public Facture(String NoFacture)
+        {
+            cmp++;
+            this.ID = cmp;
+            this.NoFacture = NoFacture;
+        }
+
+    }
+}
